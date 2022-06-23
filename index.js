bc30c2f12944c18b27958d2e5ebfd17245aeaf3c
Tucker Monroe <tmonroe19@liberty.edu>
2022-06-23 11:13:34 -0400

    why do you hate me?

---
+++ index.js
@@ -0,0 +1,110 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.end(`Phonebook has info for ${persons.length} people. ${Date()}`)
+})
+
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+const generateID = () => {
+    const maxID = persons.length > 0
+        ? Math.floor(Math.random() * 10000000000000000000)
+        : 0
+    return maxID + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    const name = body.name
+    const number = body.number
+
+    if (!name) {
+        return response.status(400).json({
+            error: 'The name is missing'
+        })
+    }
+
+    if (!number) {
+        return response.status(400).json({
+            error: 'The number is missing'
+        })
+    }
+
+    if (persons.some(element => element.name == name) ){
+        return response.status(400).json({
+            error: 'The name already exists in the phonebook'
+        })
+    }
+
+    const person = {
+        id: generateID(),
+        name: name,
+        number: number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const cors = require('cors')
+app.use(cors())
